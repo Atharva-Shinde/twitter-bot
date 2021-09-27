25d955c5c0772391610d0755ea82231c495ff1f5
Atharva Shinde <atharvashinde179@gmail.com>
2021-09-27 18:33:53 +0530

    wip

--- app.js
+++ app.js
@@ -17,3 +17,19 @@ rule.dayOfWeek = 1;
 rule.hour = 10;
 rule.tz = "Etc/GMT+5:30";
 
+// this is to creat a client i.e twit to connect to twitter Api 
+const twitter_client = require('twit');
+
+// Pulling keys from another file
+var config = require('./config.js');
+
+// this is created so that we can make requests to twitter api 
+// from now on when we want to use twit modules we'll simply reference the T.propertyName suntax
+var T = new twitter_client(config);
+
+// this stream variable filters the twitter public stream that mentions your username from other twitter accounts
+var stream = T.stream('statuses/filter', {track: twtusername});
+
+// 'tweet' is a parameter that should be listened
+// pressStartis a function that handles the events 
+stream.on('tweet', pressStart);
